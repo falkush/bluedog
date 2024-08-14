7fe44e4e3596993ae341f3e99a367df76ecec3df
falkush <115177613+falkush@users.noreply.github.com>
2024-08-14 17:46:50 -0400

    Add files via upload

---
+++ bluedogwin/bluedogwin.lua
@@ -0,0 +1,25 @@
+input = {};
+
+savestate.loadslot(1);
+for j=0,820 do
+	emu.frameadvance();
+	emu.frameadvance();
+	emu.frameadvance();
+end
+
+while true do
+	emu.frameadvance();
+	emu.frameadvance();
+	emu.frameadvance();
+
+	input['P1 A'] = true;
+	joypad.set(input);
+	emu.frameadvance();
+	input['P1 A'] = false;
+	joypad.set(input);
+	emu.frameadvance();
+
+	while true do
+		emu.frameadvance();
+	end
+end
